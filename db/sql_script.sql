5c6e109b5132a305b8498596c32936e18a681a0e
vladprosolupov <prosolupov97@gmail.com>
2017-04-18 22:47:08 +0300

    Modified sql script (added all tables)

--- db/sql_script.sql
+++ db/sql_script.sql
@@ -5,20 +5,20 @@ name varchar(128)
 
 CREATE TABLE Country(
 id int IDENTITY(1,1) PRIMARY KEY,
-name varchar(128)
+name varchar(32)
 );
 
 CREATE TABLE Actor(
 id int IDENTITY(1,1) PRIMARY KEY,
-first_name varchar(128),
-last_name varchar(128),
+first_name varchar(64),
+last_name varchar(64),
 birthdate datetime,
 id_country int not null FOREIGN KEY references Country(id)
 );
 
 CREATE TABLE Language(
 id int IDENTITY(1,1) PRIMARY KEY,
-name varchar (128)
+name varchar (32)
 );
 
 /*
@@ -52,7 +52,7 @@ link text
 
 CREATE TABLE City(
 id int IDENTITY(1,1) PRIMARY KEY,
-name varchar(128) not null,
+name varchar(64) not null,
 id_country int not null foreign key references Country(id)
 );
 
@@ -61,8 +61,8 @@ CREATE TABLE Address(
 id int IDENTITY(1,1) PRIMARY KEY,
 address text,
 address2 text,
-district varchar(128),
-postal_code varchar(64),
+district varchar(64),
+postal_code varchar(32),
 id_city int not null foreign key references City(id)
 );
 
@@ -76,12 +76,12 @@ Not enough information from entity diagram cannot fill all the fields.
 */
 CREATE TABLE Client(
 id int IDENTITY(1,1) PRIMARY KEY,
-first_name varchar(128),
-last_name varchar(128),
+first_name varchar(64),
+last_name varchar(64),
 email varchar(128),
 id_address int not null foreign key references Address(id),
 creation_date datetime,
-login varchar(128) not null unique,
+login varchar(64) not null unique,
 passwordHash binary(64) not null,
 salt UNIQUEIDENTIFIER
 );
@@ -98,5 +98,80 @@ id_film int not null foreign key references Film(id)
 );
 /*(end)*/
 
+/* To fix 
+Not enough information from entity diagram
+*/
+CREATE TABLE Comment_rating(
+id int IDENTITY(1,1) PRIMARY KEY,
+comment_text text,
+id_client int not null foreign key references Client(id),
+Film_rating float,
+comment_date datetime,
+id_film int not null foreign key references Film(id)
+);
+
+/*(end)*/
+
+CREATE TABLE Film_Category(
+id int IDENTITY(1,1) PRIMARY KEY,
+id_film int not null foreign key references Film(id),
+id_category int not null foreign key references Category(id)
+);
 
 
+CREATE TABLE Film_Actor(
+id int IDENTITY(1,1) PRIMARY KEY,
+id_film int not null foreign key references Film(id),
+id_actor int not null foreign key references Actor(id),
+role varchar(64)
+);
+
+CREATE TABLE Studio(
+id int IDENTITY(1,1) PRIMARY KEY,
+studio_name varchar(64)
+);
+
+CREATE TABLE Film_studio(
+id int IDENTITY(1,1) PRIMARY KEY,
+id_film int not null foreign key references Film(id),
+id_studio int not null foreign key references Studio(id)
+);
+
+CREATE TABLE Network(
+id int IDENTITY(1,1) PRIMARY KEY,
+network_name varchar(64),
+network_logo text
+);
+
+
+CREATE TABLE Link_to_network(
+id int IDENTITY(1,1) PRIMARY KEY,
+id_network int not null foreign key references Network(id),
+link text
+);
+
+CREATE TABLE Film_network(
+id int IDENTITY(1,1) PRIMARY KEY,
+id_film int not null foreign key references Film(id),
+id_link_to_network int not null foreign key references Link_to_network(id)
+);
+
+CREATE TABLE Director(
+id int IDENTITY(1,1) PRIMARY KEY,
+first_name varchar(64),
+last_name varchar(64),
+id_country int not null foreign key references Country(id)
+);
+
+CREATE TABLE Film_director(
+id int IDENTITY(1,1) PRIMARY KEY,
+id_film int not null foreign key references Film(id),
+id_director int not null foreign key references Director(id)
+);
+
+CREATE TABLE Award(
+id int IDENTITY(1,1) PRIMARY KEY,
+award_name varchar(64),
+award_year int,
+id_film int not null foreign key references Film(id)
+);
