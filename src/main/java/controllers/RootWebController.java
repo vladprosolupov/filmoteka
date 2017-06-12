f756124a5b6f0330e4bd7a4ac9b90fc63873e2aa
vladprosolupov <s13009@pjwstk.edu.pl>
2017-06-12 19:24:31 +0200

    Bug fixes related with configuration.

--- src/main/java/controllers/RootWebController.java
+++ src/main/java/controllers/RootWebController.java
@@ -1,12 +1,13 @@
 package controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Rostyk on 12.06.2017.
  */
-@RestController
+@Controller
 public class RootWebController {
 
     @RequestMapping(value = "/")
