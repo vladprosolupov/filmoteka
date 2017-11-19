98d5abb305e6c6b0145dee2536cc1578409581e5
rostyslavT <s12864@pjwstk.edu.pl>
2017-11-19 15:18:13 +0100

    Added logs to services

--- src/main/java/web/services/CategoryService.java
+++ src/main/java/web/services/CategoryService.java
@@ -24,7 +24,7 @@ public class CategoryService {
     @Autowired(required = true)
     private SessionFactory sessionFactory;
 
-    private static final Logger log = LogManager.getLogger(AwardService.class);
+    private static final Logger log = LogManager.getLogger(CategoryService.class);
 
     public List<CategoryDb> getAllCategories() throws HibernateException {
         log.info("getAllCategories()");
@@ -36,7 +36,7 @@ public class CategoryService {
         return result;
     }
 
-    public List<CategoryJSON> getForNav(){
+    public List<CategoryJSON> getForNav() {
         log.info("getForNav()");
 
         Session session = sessionFactory.getCurrentSession();
@@ -48,37 +48,61 @@ public class CategoryService {
     }
 
     public CategoryDb getCategoryWithId(String id) throws HibernateException, IndexOutOfBoundsException {
+        log.info("getCategoryWithId(id=" + id + ")");
+
         if (id == null || id.isEmpty()) {
+            log.error("Error : id is incorrect");
+
             throw new IllegalArgumentException("Id should not be null or empty");
         }
         Session session = sessionFactory.getCurrentSession();
         CategoryDb categoryDb = (CategoryDb) session.createQuery("from CategoryDb c where c.id=" + id).list().get(0);
+
+        log.info("getCategoryWithId() returns : categoryDb=" + categoryDb);
         return categoryDb;
     }
 
     public void saveOrUpdate(CategoryDb categoryDb) throws HibernateException {
+        log.info("saveOrUpdate(categoryDb=" + categoryDb + ")");
+
         if (categoryDb == null) {
+            log.error("Error : categoryDb is null");
+
             throw new IllegalArgumentException("CategoryDb should not be null");
         }
         Session session = sessionFactory.getCurrentSession();
         session.saveOrUpdate(categoryDb);
+
+        log.info("succ. saved or updated category");
     }
 
     public void deleteCategory(String id) throws HibernateException {
+        log.info("deleteCategory(id=" + id + ")");
+
         if (id == null || id.isEmpty()) {
+            log.error("Error : id is incorrect");
+
             throw new IllegalArgumentException("Id should not be null or empty");
         }
         Session session = sessionFactory.getCurrentSession();
         session.createQuery("delete from CategoryDb c where c.id=" + id).executeUpdate();
+
+        log.info("succ. deleted category");
     }
 
     public CategoryDb convertToCategoryDb(CategoryJSON categoryJSON) throws ParsingJsonToDaoException {
+        log.info("convertToCategoryDb(categoryJSON=" + categoryJSON + ")");
+
         if (categoryJSON == null) {
+            log.error("Error : categoryJSON is null");
+
             throw new IllegalArgumentException("CategoryJSON should not be null");
         }
         CategoryDb categoryDb = new CategoryDb();
         categoryDb.setId(categoryJSON.getId());
         categoryDb.setName(categoryJSON.getName());
+
+        log.info("convertTiCategoryDb() retuns : categoryDb=" + categoryDb);
         return categoryDb;
     }
 }
--- src/main/java/web/services/ClientService.java
+++ src/main/java/web/services/ClientService.java
@@ -1,5 +1,7 @@
 package web.services;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,48 +29,82 @@ public class ClientService {
     @Autowired(required = true)
     private AddressService addressService;
 
+    private static final Logger log = LogManager.getLogger(ClientService.class);
+
     public void saveOrUpdate(ClientDb clientDb) throws HibernateException {
+        log.info("saveOrUpdate(clientDb=" + clientDb + ")");
+
         if (clientDb == null) {
+            log.error("Error : cientDb is null");
+
             throw new IllegalArgumentException("ClientDb should not be null");
         }
         Session session = sessionFactory.getCurrentSession();
         session.saveOrUpdate(clientDb);
+
+        log.info("succ. saved or updated client");
     }
 
     public ClientDb getClientByLogin(String login) throws HibernateException, IndexOutOfBoundsException {
+        log.info("getClientByLogin(login=" + login + ")");
+
         if (login == null || login.isEmpty()) {
+            log.error("Error : login is incorrect");
+
             throw new IllegalArgumentException("Login should not be null");
         }
         Session session = sessionFactory.getCurrentSession();
         ClientDb client = (ClientDb) session.createQuery("FROM ClientDb c where c.login='" + login + "'").list().get(0);
+
+        log.info("getClientByLogin() returns : client=" + client);
         return client;
     }
 
     public List<ClientDb> getAll() throws HibernateException {
+        log.info("getAll()");
+
         Session session = sessionFactory.getCurrentSession();
         List<ClientDb> result = session.createQuery("from ClientDb").list();
+
+        log.info("getAll() returns : result.size()=" + result.size());
         return result;
     }
 
     public void delete(String id) throws HibernateException {
+        log.info("delete(id=" + id + ")");
+
         if (id == null || id.isEmpty()) {
+            log.error("Error : id is incorrect");
+
             throw new IllegalArgumentException("Id should not be null or empty");
         }
         Session session = sessionFactory.getCurrentSession();
         session.createQuery("delete from ClientDb c where c.id=" + id).executeUpdate();
+
+        log.info("succ. deleted client");
     }
 
     public ClientDb getClientById(String id) throws HibernateException, IndexOutOfBoundsException {
+        log.info("getClientById(id=" + id + ")");
+
         if (id == null || id.isEmpty()) {
+            log.error("Error : id is incorrect");
+
             throw new IllegalArgumentException("Id should not be null or empty");
         }
         Session session = sessionFactory.getCurrentSession();
         ClientDb clientDb = (ClientDb) session.createQuery("from ClientDb c where c.id=" + id).list().get(0);
+
+        log.info("getClientById() returns : clientDb=" + clientDb);
         return clientDb;
     }
 
     public ClientDb convertToClientDb(ClientJSON clientJSON) throws ParsingJsonToDaoException, IllegalArgumentException {
+        log.info("convertToClientDb(clientJSON=" + clientJSON + ")");
+
         if (clientJSON == null) {
+            log.error("Error : clientJSON is null");
+
             throw new IllegalArgumentException("ClientJSON should not be null");
         }
 
@@ -85,6 +121,7 @@ public class ClientService {
         clientDb.setCreationDate(new Timestamp(System.currentTimeMillis()));
 //        clientDb.setAddressByIdAddress(addressService.getAddressById(clientJSON.getIdAddress()));
 
+        log.info("convertToClientDb() returns : clientDb=" + clientDb);
         return clientDb;
     }
 }
